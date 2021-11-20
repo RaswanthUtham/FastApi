c4a692f19ee615042d166ea45c6e20c7154eac21
Raswanth <u.raswanth@gmail.com>
2021-11-21 00:20:57 +0530

    added prefix in router to avoid repeating the url paths in FastApi decorators

--- app/router/post.py
+++ app/router/post.py
@@ -5,14 +5,14 @@ from typing import List
 from .. import models, schemas
 from ..database import get_db
 
-router = APIRouter()
+router = APIRouter(prefix="/posts")
 
-@router.get("/posts", response_model=List[schemas.Post])
+@router.get("/", response_model=List[schemas.Post])
 async def get_posts(db: Session=Depends(get_db)):
     posts = db.query(models.Post).all()
     return posts
 
-@router.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
 async def create_post(post: schemas.PostCreate, db: Session=Depends(get_db)):
     new_post = models.Post(**post.dict())
     db.add(new_post)
@@ -20,14 +20,14 @@ async def create_post(post: schemas.PostCreate, db: Session=Depends(get_db)):
     db.refresh(new_post)
     return  new_post
 
-@router.get("/posts/{id}", response_model=schemas.Post)
+@router.get("/{id}", response_model=schemas.Post)
 async def get_post_by_id(id: int, db: Session=Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with {id} not found")
     return post
 
-@router.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_post_by_id(id: int, db: Session=Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == id)
     if not post.first():
@@ -36,7 +36,7 @@ async def delete_post_by_id(id: int, db: Session=Depends(get_db)):
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-@router.put("/posts/{id}", response_model=schemas.Post)
+@router.put("/{id}", response_model=schemas.Post)
 async def update_post_by_id(id: int, post: schemas.PostCreate, db: Session=Depends(get_db)):
     update_post = db.query(models.Post).filter(models.Post.id == id)
     if not update_post.first():
