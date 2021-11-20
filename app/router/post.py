206805dda6736942be0da56e67ad94b01171359b
Raswanth <u.raswanth@gmail.com>
2021-11-21 03:39:22 +0530

    added authentication with jwt token

--- app/router/post.py
+++ app/router/post.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends, status, HTTPException, Response, APIRouter
 from sqlalchemy.orm import Session
 from typing import List
 
-from .. import models, schemas
+from .. import models, schemas, oauth2
 from ..database import get_db
 
 router = APIRouter(prefix="/posts", tags=["Posts"])
@@ -13,7 +13,8 @@ async def get_posts(db: Session=Depends(get_db)):
     return posts
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-async def create_post(post: schemas.PostCreate, db: Session=Depends(get_db)):
+async def create_post(post: schemas.PostCreate, db: Session=Depends(get_db), user: int=Depends(oauth2.get_current_user)):
+    print(user.email)
     new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
@@ -28,7 +29,7 @@ async def get_post_by_id(id: int, db: Session=Depends(get_db)):
     return post
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_post_by_id(id: int, db: Session=Depends(get_db)):
+async def delete_post_by_id(id: int, db: Session=Depends(get_db), user: int=Depends(oauth2.get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == id)
     if not post.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with {id} not found")
@@ -37,7 +38,7 @@ async def delete_post_by_id(id: int, db: Session=Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @router.put("/{id}", response_model=schemas.Post)
-async def update_post_by_id(id: int, post: schemas.PostCreate, db: Session=Depends(get_db)):
+async def update_post_by_id(id: int, post: schemas.PostCreate, db: Session=Depends(get_db), user: int=Depends(oauth2.get_current_user)):
     update_post = db.query(models.Post).filter(models.Post.id == id)
     if not update_post.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with {id} not found")
--- app/utils.py
+++ app/utils.py
@@ -4,3 +4,6 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto") # password enc
 
 def hash(data):
     return pwd_context.hash(data)
+
+def verify(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
