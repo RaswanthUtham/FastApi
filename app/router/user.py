2da38dbd8ce072d2754ebbb3666b203bdf966b1a
Raswanth <u.raswanth@gmail.com>
2021-11-21 00:34:11 +0530

    Added docs and the api requests will be grouped in docs

--- app/router/user.py
+++ app/router/user.py
@@ -5,7 +5,7 @@ from typing import List
 from .. import models, schemas, utils
 from ..database import get_db
 
-router = APIRouter(prefix="/users")
+router = APIRouter(prefix="/users", tags=["Users"])
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserPost)
 async def create_user(user: schemas.UserCreate, db:Session=Depends(get_db)):
