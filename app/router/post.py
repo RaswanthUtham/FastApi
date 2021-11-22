69fcabb5ddaa5d78f0d6a85aa5919107d67d470b
Raswanth <u.raswanth@gmail.com>
2021-11-22 19:56:26 +0530

    added security to not delete / update a post by other users

--- app/router/post.py
+++ app/router/post.py
@@ -29,18 +29,24 @@ async def get_post_by_id(id: int, db: Session=Depends(get_db)):
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_post_by_id(id: int, db: Session=Depends(get_db), user: int=Depends(oauth2.get_current_user)):
-    post = db.query(models.Post).filter(models.Post.id == id)
-    if not post.first():
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+    if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with {id} not found")
-    post.delete(synchronize_session=False)
+    if post.owner_id != user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to delete this post")
+    post_query.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @router.put("/{id}", response_model=schemas.Post)
 async def update_post_by_id(id: int, post: schemas.PostCreate, db: Session=Depends(get_db), user: int=Depends(oauth2.get_current_user)):
-    update_post = db.query(models.Post).filter(models.Post.id == id)
-    if not update_post.first():
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post_to_update = post_query.first()
+    if not post_to_update:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with {id} not found")
-    update_post.update(post.dict(), synchronize_session=False)
+    if post_to_update.owner_id != user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to delete this post")
+    post_query.update(post.dict(), synchronize_session=False)
     db.commit()
-    return update_post.first()
+    return post_query.first()
