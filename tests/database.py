677ce3f1d98fe407c8f72faf1621b429b20d4207
Raswanth <u.raswanth@gmail.com>
2021-11-25 16:40:46 +0530

    changed the scope of fixture from module to function
        To avoid dependemcies between testcases
        Improved the login test checks

--- tests/database.py
+++ tests/database.py
@@ -14,7 +14,7 @@ engine = create_engine(
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base.metadata.create_all(bind=engine)
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def session():
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
@@ -24,7 +24,7 @@ def session():
     finally:
         db.close()
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def client(session):
     def override_get_db():
         try:
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,19 @@
+from pydantic.types import Json
+from jose import jwt
 import pytest
 from app import schemas
+from app.config import settings
 from .database import client, session
+
+@pytest.fixture
+def test_user(client):
+    user_data = {"email": "magizhan_01@gmail.com", "password": "test@123"}
+    res = client.post("/users/", json=user_data)
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
+
 
 def test_root(client):
     res = client.get("/")
@@ -14,6 +27,11 @@ def test_create_user(client):
     assert user.email == "magizhan_01@gmail.com"
     assert res.status_code == 201
 
-def test_login_user(client):
-    res = client.post("/login", data={"username": "magizhan_01@gmail.com", "password": "test@123"})
+def test_login_user(test_user, client):
+    res = client.post("/login", data={"username": test_user['email'], "password": test_user['password']})
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token, settings.secret_key, algorithms=[settings.algorithm])
+    id = payload.get("user_id")
+    assert id == test_user["id"]
+    assert login_res.token_type == "bearer"
     assert res.status_code == 200
