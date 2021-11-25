6674fbc71c5dbcf1acc22e34d5e6435abd5ba656
Raswanth <u.raswanth@gmail.com>
2021-11-25 16:50:45 +0530

    Implemented confest file
        Contents within this file can be used by all the test files within
        the test package.

---
+++ tests/conftest.py
@@ -0,0 +1,35 @@
+import pytest
+from fastapi.testclient import TestClient
+from app.database import get_db
+from app.main_orm import app
+from app.database import Base
+from .database import engine, TestingSessionLocal
+
+@pytest.fixture(scope="function")
+def session():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@pytest.fixture(scope="function")
+def client(session):
+    def override_get_db():
+        try:
+            yield session
+        finally:
+            session.close()
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
+
+@pytest.fixture
+def test_user(client):
+    user_data = {"email": "magizhan_01@gmail.com", "password": "test@123"}
+    res = client.post("/users/", json=user_data)
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
--- tests/test_users.py
+++ tests/test_users.py
@@ -3,17 +3,6 @@ from jose import jwt
 import pytest
 from app import schemas
 from app.config import settings
-from .database import client, session
-
-@pytest.fixture
-def test_user(client):
-    user_data = {"email": "magizhan_01@gmail.com", "password": "test@123"}
-    res = client.post("/users/", json=user_data)
-    assert res.status_code == 201
-    new_user = res.json()
-    new_user['password'] = user_data['password']
-    return new_user
-
 
 def test_root(client):
     res = client.get("/")
